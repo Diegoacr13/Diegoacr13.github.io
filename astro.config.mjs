2a4ea4b7b06f979feb36abdcc3bd8e3bed7a5b80
Diegoacr13 <diegoa.contreras1305@gmail.com>
2026-03-21 00:22:04 -0600

    Deploy y site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: Diegoacr13.github.io,
     vite: {
         plugins: [tailwindcss()]
     },
