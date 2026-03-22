88546207a2b2a1edf385e3f62b080484dfa884f5
Diegoacr13 <diegoa.contreras1305@gmail.com>
2026-03-22 01:25:01 -0600

    comptencies4

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -43,7 +43,17 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Hi There & Welcome to My Portfolio',
-        text: "I'm Diego A. Contreras, Licensed architect and data analyst specialized in project management and process optimization.\nExpert in information visualization and team coordination, applying project management methodologies in construction and technology.\n Led the management of the Cancún–Nichupté bridge project,implementing dashboards and communication plans that improved collaboration and accelerated construction timelines.\n Core COmpetencies: \n \nPython: Data cleaning, analysis, and visualization using libraries such as Pandas, NumPy, Matplotlib, and Seaborn \n\n\n\n \n",
+        text: `I'm Diego A. Contreras, Licensed architect and data analyst specialized in project management and process optimization.
+            Expert in information visualization and team coordination, applying project management methodologies in construction and technology.
+            Led the management of the Cancún–Nichupté bridge project, implementing dashboards and communication plans that improved collaboration and accelerated construction timelines.
+
+            **Lista de competencias:**
+            - Data Analysis
+            - Project Management
+            - Process Optimization
+            - Information Visualization
+            - Dashboard Design
+            `,
         image: {
             src: "/foto-perfil.png",
             alt: 'Foto Diego A. Contreras Robles'
