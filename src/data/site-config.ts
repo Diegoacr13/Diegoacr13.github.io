e292d6584030142666aaba0c0defe095a2d6e7ed
Diegoacr13 <diegoa.contreras1305@gmail.com>
2026-03-21 20:11:26 -0600

    config de regreos

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -2,7 +2,11 @@ import type { SiteConfig } from '../types';
 
 const siteConfig: SiteConfig = {
     website: 'https://diegocontreras1305.github.io',
+    avatar: {
+        src: '/foto-perfil.png',
+    alt: 'Foto de Diego A. Contreras Robles'
 
+    },
     title: 'Diego A. Contreras Robles',
     subtitle: 'Data Analyst | Project Manager | Process Optimization and Information Visualization | Dashboards',
     description: 'Portafolio de Diego A. Contreras Robles — Analista de Datos y Project Manager especializado en optimización de procesos, dashboards y visualización de información.',
@@ -38,20 +42,19 @@ const siteConfig: SiteConfig = {
         },
         {
             text: 'Resume',
-            href: 'file:///C:/Users/HP%20Victus/Desktop/Sprint/Sprint%209/CV%20DATA%20DACR_eng.pdf'
+            href: 'https://instagram.com/'
         },
         {
             text: 'CV',
-            href: 'file:///C:/Users/HP%20Victus/Desktop/Sprint/Sprint%209/Diego_Contreras_CV.pdf'
+            href: 'https://instagram.com/'
         }
     ],
     hero: {
         title: 'Hi There & Welcome to My Portfolio',
-        text: "I'm Diego Comtreras,Licensed architect and data analyst specialized in project management and process optimization. Expert in information visualization and team coordination, applying project management methodologies in construction and echnology. Led the management of the Cancún.Nichupté bridge project, implementing dashboards and communication plans that improved collaboration and accelerated construction timelines."
+        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence.\nMy approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products.\nI have a profound appreciation for top-notch software, visual design, and the principles of product-led growth.\n\nFeel free to explore some of my coding endeavors on [GitHub](https://github.com/JustGoodUI/dante-astro-theme) or follow me on [Twitter/X](https://twitter.com/justgoodui).",
         image: {
-            src: '/foto-perfil.png',
-            alt: 'Foto de Diego A. Contreras Robles'
-         },
+            src: hero,
+            alt: 'A person sitting at a desk in front of a computer'
         },
         actions: [
             {
